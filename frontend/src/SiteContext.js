eaf7c25fed121eefaab9ee9875b14b2a0cf48d48
Min ho Kim <minho42@gmail.com>
2021-11-20 22:43:19 +1100

    return newly added prevSymbols

--- frontend/src/SiteContext.js
+++ frontend/src/SiteContext.js
@@ -103,6 +103,7 @@ export const SiteProvider = ({ children }) => {
         setErrorMessage,
         fetchEquityPositions,
         transactionHistory,
+        prevSymbols,
       }}
     >
       {children}
