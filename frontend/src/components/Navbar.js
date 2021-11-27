b8ac2c44d771f2877146ee73493b9afdf37b4ae8
Min ho Kim <minho42@gmail.com>
2021-11-28 00:38:57 +1100

    remove logout svg

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -79,20 +79,6 @@ export const Navbar = () => {
               className="flex items-center h-full font-medium px-2 sm:px-4 py-3 cursor-pointer border-b-4 hover:bg-gray-100 capitalize gap-1 border-transparent"
             >
               logout
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                />
-              </svg>
             </button>
           )}
           <Link
