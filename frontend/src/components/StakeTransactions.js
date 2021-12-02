162d79424329d2bfb3e18b4d8edfa3b4c2b798e9
Min ho Kim <minho42@gmail.com>
2021-12-03 01:31:28 +1100

    transaction "x months ago" & style

--- frontend/src/components/StakeTransactions.js
+++ frontend/src/components/StakeTransactions.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { showValueWithComma, timestampToDate, dateStrToTimestamp } from "../utils";
+import { formatDistance } from "date-fns";
 
 export const StakeTransactions = ({ transactions }) => {
   const [prevDotID, setPrevDotID] = useState(null);
@@ -37,26 +38,35 @@ export const StakeTransactions = ({ transactions }) => {
           <div
             onMouseOver={() => handleHover(t.orderID)}
             key={t.orderID}
-            className="flex flex-col items-center text-gray-500 px-1 py-0.5 hover:bg-gray-100 gap-1 cursor-pointer"
+            className="flex flex-col items-center px-2 py-0.5 hover:bg-gray-100 gap-1 cursor-pointer"
           >
-            <div className="flex w-44 items-center gap-1">
-              <div
-                className={`${
-                  t.transactionType.toLowerCase() === "buy" ? "bg-green-600" : "bg-red-500"
-                } flex items-center justify-center  w-4 h-4 text-white`}
-              >
-                {`${t.transactionType.toLowerCase() === "buy" ? "B" : "S"}`}
+            <div className="flex w-72 items-start justify-between gap-1">
+              <div className="flex items-center">
+                <div
+                  className={`${
+                    t.transactionType.toLowerCase() === "buy" ? "bg-green-600" : "bg-red-500"
+                  } flex items-center justify-center  w-4 h-4 text-white`}
+                >
+                  {`${t.transactionType.toLowerCase() === "buy" ? "B" : "S"}`}
+                </div>
+                <div
+                  className={`${
+                    t.transactionType.toLowerCase() === "buy" ? "text-green-600" : "text-red-600"
+                  } flex px-1 text-base`}
+                >
+                  {showValueWithComma(Math.abs(t.tranAmount))}
+                </div>
               </div>
-              <div>{timestampToDate(dateStrToTimestamp(t.timestamp))}:</div>
-              <div
-                className={`${
-                  t.transactionType.toLowerCase() === "buy" ? "text-green-600" : "text-red-600"
-                } flex px-1`}
-              >
-                {showValueWithComma(Math.abs(t.tranAmount))}
+              <div>
+                {formatDistance(new Date(t.timestamp), new Date(), {
+                  addSuffix: true,
+                })}
+                <span className="ml-1 text-gray-500">
+                  ({timestampToDate(dateStrToTimestamp(t.timestamp))})
+                </span>
               </div>
             </div>
-            <div className=" text-sm text-gray-400 px-2 pb-0.5">{t.comment}</div>
+            <div className="text-xs text-gray-500 px-2 pb-0.5">{t.comment}</div>
           </div>
         );
       })}
