3acac67b51c9c00027389ede12a8bf5130b8dd15
Min ho Kim <minho42@gmail.com>
2022-08-31 11:39:00 +1000

    remove twitter, github links

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -9,27 +9,6 @@ export const About = () => {
       <div className="flex justify-center py-3 text-center">
         <div className="flex flex-col justify-center max-w-md w-full text-2xl">Stake</div>
       </div>
-      <div className="flex justify-center text-center">
-        <a
-          className="underline"
-          href="https://github.com/minho42/stake"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github
-        </a>
-      </div>
-
-      <div className="flex justify-center text-center">
-        <a
-          className="underline"
-          href="https://twitter.com/minhokim42"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          twitter
-        </a>
-      </div>
 
       <HeatMap transactionHistory={transactionHistory} />
     </div>
